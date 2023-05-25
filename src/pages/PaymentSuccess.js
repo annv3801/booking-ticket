96ec2d6ded5db595edc875df7f3eedbfededf4e0
annv <annv@nobisoft.com.vn>
2023-05-25 19:33:26 +0700

    wip

--- src/pages/PaymentSuccess.js
+++ src/pages/PaymentSuccess.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import Header from "../components/layout/Header";
 import axios from "axios";
-import {useNavigate} from "react-router-dom";
+import {NavLink, useNavigate} from "react-router-dom";
 
 const PaymentSuccess = () => {
     const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
@@ -42,13 +42,20 @@ const PaymentSuccess = () => {
     };
     axios.post("http://localhost:5233/booking", data, config)
         .then(res => {
-            navigate("/");
         });
 
     return (
-        <div>
-           <Header></Header>
-            Thanh toán thành công
+        <div className="h-[100vh] flex items-center justify-center flex-col md:px-5">
+            <div className="max-w-[700px] mx-auto text-center">
+                <div>
+                    <img src="https://www.cgv.vn/skin/frontend/cgv/default/images/cgvlogo.png" className="mx-auto mb-5"
+                         alt="Flowbite Logo"/>
+                    <div className="font-bold text-3xl mb-10">
+                        Thanh toán thành công
+                    </div>
+                    <NavLink to={"/"} className="bg-green-400 text-white mt-5 px-3 py-5 rounded-lg text-xl">Quay trở lại trang chủ</NavLink>
+                </div>
+            </div>
         </div>
     );
 };
